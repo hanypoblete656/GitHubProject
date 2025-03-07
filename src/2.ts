4fbfd26f73ec07855cfcd89718c482d05f2fbd58
hanypoblete656 <hanypoblete656@gmail.com>
2025-03-07 04:20:48 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,12 @@
+// This is a comment
+let name = "John Doe";
+console.log(name);
+
+// Arrow function expression
+const add = (a: number, b: number): number => {
+  return a + b;
+}
+
+// Object destructuring
+const person = { name: "John", age: 30 };
+const { name } = person; // name is now equal to "John"
